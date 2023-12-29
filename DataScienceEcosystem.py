c0a2f7ff932643ff3d3d64165e930dab81c747ce
afonsocabral1 <150057366+afonsocabral1@users.noreply.github.com>
2023-12-29 21:03:34 +0000

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# 
+#     - List popular languages for Data Science
+#     - List commonly used libraries used by Data Scientists
+#     - Outline 3 of the most widely used development environment tools in Data Science
+#     - Provide some examples of arithmetic expressions used in Python.
+
+# Some of the popular languages that Data Scientists use are:
+#     
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. JavaScript
+# 5. Julia
+# 6. Java
+# 7. Scala
+# 8. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. SciKit-Learn
+# 6. Keras
+# 7. TensorFlow
+# 8. Pytorch
+
+# | Data Science Tools |
+# |--------------------|
+# |Jupyter Notebooks |
+# |RStudio|
+# |Spyder|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# Example 1: Finding the value of 4 to the power of 3:
+# 
+# x=pow(4,3)
+# print(x)
+# 
+# 64
+# 
+# Example 2: Finding the multiplication of 2 numbers:
+# 
+# 2*3
+# 
+# 6
+# 
+# Example 3: Finding the subtraction of 2 numbers:
+# 
+# 4-1
+# 
+# 3
+
+# In[2]:
+
+
+# This is a simple arithmetic expression to multiply then add numbers.
+
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+
+200/60
+
+
+# ## Author
+# Afonso de Melo
+
+# In[ ]:
+
+
+
+
